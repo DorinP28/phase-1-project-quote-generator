eaec849447a7db6c3fb66a30670c03103fd92f03
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-05-14 03:48:28 -0400

    js add newQuote function, fetch the api for qoutes and selecting/updating the content of HTML document with quote and author

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const button = document.getElementById("new-quote-button");
 
 // Add an event listener to the button
 button.addEventListener("click", newQuote);
+
+function newQuote() {
+    fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(data => {
+        const quote = data.content;
+        const author = data.author;
+  
+        document.getElementById("quote").textContent = quote;
+        document.getElementById("author").textContent = author;
+  
+      })
+      .catch(error => console.error(error));
+  }
+  
