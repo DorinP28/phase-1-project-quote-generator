96cdac5d31288fe968dfbfded10c566cd5187e25
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-05-14 03:58:39 -0400

    js add feature for the image background

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const button = document.getElementById("new-quote-button");
 // Add an event listener to the button
 button.addEventListener("click", newQuote);
 
+// Define the newQuote function and fetch to API
 function newQuote() {
     fetch("https://api.quotable.io/random")
       .then(response => response.json())
@@ -14,7 +15,13 @@ function newQuote() {
         document.getElementById("quote").textContent = quote;
         document.getElementById("author").textContent = author;
   
+        //generate a random number between 1 and 12 (inclusive) 
+        const randomImage = Math.floor(Math.random() * 12) + 1;
+        //connect the randomImage to an image(1-12).jpg from src folder to the body element in HTML for the background
+        document.body.style.backgroundImage = `url('src/picture${randomImage}.jpg')`;
       })
       .catch(error => console.error(error));
   }
+
+  
   
