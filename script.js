3b7b155762a35bbd82f2da9d5ce92370907122cf
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-05-14 03:36:43 -0400

    js add eventListener to the button

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // Get a reference to the HTML button element
 const button = document.getElementById("new-quote-button");
+
+// Add an event listener to the button
+button.addEventListener("click", newQuote);
